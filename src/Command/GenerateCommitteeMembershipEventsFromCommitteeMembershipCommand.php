ac6685ebdc27e0dc23fc5f7a1fd597c601143a38
Benoit Galati <benoit.galati@gmail.com>
2018-04-25 10:18:33 +0200

    Optimize command for large result set

--- src/Command/GenerateCommitteeMembershipEventsFromCommitteeMembershipCommand.php
+++ src/Command/GenerateCommitteeMembershipEventsFromCommitteeMembershipCommand.php
@@ -6,7 +6,6 @@ use AppBundle\Entity\CommitteeMembership;
 use AppBundle\Entity\Reporting\CommitteeMembershipAction;
 use AppBundle\Entity\Reporting\CommitteeMembershipEvent;
 use AppBundle\Referent\ManagedAreaUtils;
-use AppBundle\Repository\CommitteeMembershipRepository;
 use AppBundle\Repository\CommitteeRepository;
 use AppBundle\Repository\ReferentTagRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -23,18 +22,16 @@ class GenerateCommitteeMembershipEventsFromCommitteeMembershipCommand extends Co
      * @var SymfonyStyle
      */
     private $io;
-    private $committeeMembershipRepository;
     private $committeeRepository;
     private $referentTagRepository;
     private $em;
 
     const BATCH_SIZE = 30;
 
-    public function __construct(CommitteeMembershipRepository $committeeMembershipRepository, CommitteeRepository $committeeRepository, ReferentTagRepository $referentTagRepository, EntityManagerInterface $em)
+    public function __construct(CommitteeRepository $committeeRepository, ReferentTagRepository $referentTagRepository, EntityManagerInterface $em)
     {
         parent::__construct();
 
-        $this->committeeMembershipRepository = $committeeMembershipRepository;
         $this->em = $em;
         $this->committeeRepository = $committeeRepository;
         $this->referentTagRepository = $referentTagRepository;
@@ -53,10 +50,12 @@ class GenerateCommitteeMembershipEventsFromCommitteeMembershipCommand extends Co
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var CommitteeMembership $membership */
-        $memberships = $this->committeeMembershipRepository->findAll();
+        $memberships = $this->findAllCommitteeMembership();
 
-        $this->io->progressStart(count($memberships));
+        $this->io->progressStart($this->countCommitteeMembership());
+
         foreach ($memberships as $i => $membership) {
+            $membership = $membership[0];
             $this->io->progressAdvance();
             $committee = $this->committeeRepository->findOneByUuid($membership->getCommitteeUuid()->toString());
 
@@ -79,6 +78,7 @@ class GenerateCommitteeMembershipEventsFromCommitteeMembershipCommand extends Co
             }
 
             $this->em->persist($event);
+            $this->em->detach($membership);
 
             if (0 === ($i % self::BATCH_SIZE)) {
                 $this->em->flush();
@@ -110,6 +110,29 @@ class GenerateCommitteeMembershipEventsFromCommitteeMembershipCommand extends Co
             ->setParameter('tag', $event->getTag())
             ->setParameter('privilege', $event->getPrivilege())
             ->setParameter('date', $event->getDate())
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+    }
+
+    /**
+     * @return \Doctrine\ORM\Internal\Hydration\IterableResult
+     */
+    protected function findAllCommitteeMembership(): \Doctrine\ORM\Internal\Hydration\IterableResult
+    {
+        return $this->em->createQueryBuilder()
+            ->select('membership')
+            ->from(CommitteeMembership::class, 'membership')
+            ->getQuery()
+            ->iterate()
+        ;
+    }
+
+    private function countCommitteeMembership(): int
+    {
+        return $this->em->createQueryBuilder()
+            ->select('count(membership)')
+            ->from(CommitteeMembership::class, 'membership')
             ->getQuery()
             ->getSingleScalarResult()
         ;
